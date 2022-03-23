2c857cdacd3e992e834e83c596ed73f55ed39b15
JonaMaz <jonathon.mazurek@gmail.com>
2022-03-23 15:38:30 -0400

    Added Args for Reverse and for Specifying a File
    Added the Following Command
    --reverse
    Which causes the output to be reversed
    Also Added the ability to pick any file instead of just SortMe.txt
    
    Fixes:
    I forgot how arrays worked, and had it checking the 2nd and 3rd character in a word. This lead to the list not being sorted correctly. The program now correctly checks the 1st and 2nd letter.

--- NameSorter.py
+++ NameSorter.py
@@ -1,40 +1,56 @@
 #2d Array. With each internal array containing all words of the same length
 #The focus here is to get something working now. 
+import argparse
+import string
+parser = argparse.ArgumentParser(description="Sorts the Names in a given folder, Based on Size and Letters")
+parser.add_argument("file", help="What File to sort. With out this Argument it will default to \'SortMe.txt\'.", nargs="?",metavar="File",default="./Sort Me.txt")
+parser.add_argument("--reverse",dest="reversed", help="Add this to Reverse the output.", action="store_const",const=True)
 wordArray = [[]]
 bigNum = 0
+args = parser.parse_args()
+fileLoc = args.file
 def selectionSort(wordArray):
-    for i in range(len(wordArray)):
+    for i in range(0,len(wordArray)):
         index = i
         #Selection Sort
         for y in range(i+1,len(wordArray)):
-            if(ord(wordArray[index][1:2]) > ord(wordArray[y][1:2])):
+            if(ord(wordArray[index][0:1]) > ord(wordArray[y][0:1])):
                 index = y
             #If they are Equal we check the next character. if those are Equal. God have mercy on our soul
-            if(ord(wordArray[index][1:2]) == ord(wordArray[y][1:2])):
-                if(ord(wordArray[index][2:3]) > ord(wordArray[y][2:3])):
+            if(ord(wordArray[index][0:1]) == ord(wordArray[y][0:1])):
+                if(ord(wordArray[index][1:2]) > ord(wordArray[y][1:2])):
                     index = y
         temp = wordArray[index]
         wordArray[index] = wordArray[i]
         wordArray[i] = temp
     return wordArray
 
-with open("./Sort Me.txt","r") as sortFile:
+with open(fileLoc,"r") as sortFile:
     #First I find the biggest word in the file
     for words in sortFile:
         if(len(words)> bigNum):
             bigNum = len(words)
     for i in range (1,bigNum+1):
         wordArray.append([])
+
     #Get all Words into seperate arrays.
-with open("./Sort Me.txt","r") as sortFile:
+with open(fileLoc,"r") as sortFile:
     for word in sortFile:
         if(word != '\n'):
-            wordArray[len(word)].append(word)
-    #Implement a real sort instead of this. As it does not work  
+            #Word Size determines what Array it goes into. 
+            wordArray[len(word.strip())].append(word.strip())
+    #Implement a real sort instead of this. As it does not work Fully.  
     for wordSize in wordArray:
         wordSize =  selectionSort(wordSize)
-with open("./output.txt","w") as outputFile:
-    for wordRows in wordArray:
-        for words in wordRows:
-            outputFile.write(words.strip()+"\n")
+if(not args.reversed):
+    with open("./output.txt","w") as outputFile:
+        for wordRows in wordArray:
+            for words in wordRows:
+                outputFile.write(words+"\n")
+else:
+    with open("./output.txt","w") as outputFile:
+        for wordRows in reversed(wordArray):
+            for words in reversed(wordRows):
+                outputFile.write(words.strip()+"\n")
+print("File is Sorted!")
 
--- README.md
+++ README.md
@@ -6,6 +6,10 @@ open a terminal in this folder and type
 "python NameSorter.py"
 or 
 "python3 NameSorter.py" 
+Optionally you can put a file addess at the end to point it at a specific file.
 Output will be in the "output.txt" file.
 
+# Commands
+"python3 NameSorter.py --reverse"
+The ooutput will be flipped. So Larger words will be first, starting with W.
 
